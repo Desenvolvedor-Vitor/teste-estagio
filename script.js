9b0f2b991b93db8a81d002d641313824dfa15c18
seu-usersaneme <seu-email~>
2023-03-16 21:42:33 -0300

    teste-estagio

---
+++ script.js
@@ -0,0 +1,9 @@
+const indice = 13;
+var soma = 0;
+var k = 0;
+
+while(k<indice){
+  var k= k+1
+  var soma= soma+k
+}
+alert(soma)
---
+++ script02.js
@@ -0,0 +1,19 @@
+var teste = prompt("Digite um número: ")
+var num1 = 0
+var num2 = 1
+var soma= 0
+
+while(teste>soma){
+  var soma= num1+num2
+  var num1= num2
+  var num2 = soma
+ }if (soma==teste){
+    alert("Esté número faz parte da sequência Fibanacci")
+      }else{
+        alert('O número digitado não faz parte da sequência de Fibonacci.')
+       }
+      
+  
+
+
+
